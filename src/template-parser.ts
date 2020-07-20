401e589401b9fc56233285058f151a096a4f1f31
Paul Popat <p.t.popat@googlemail.com>
2020-07-20 21:48:58 +0100

    Fixed children

--- src/template-parser.ts
+++ src/template-parser.ts
@@ -44,12 +44,12 @@ function GetPropsData(attributes: NamedNodeMap | undefined, props: any): any {
 function CreateElementsFromHTML(document: Document, htmlString: string) {
   var div = document.createElement("div");
   div.innerHTML = htmlString.trim();
-  const result = div.children;
+  const result = div.childNodes;
   if (!result) {
     throw new Error("Invalid html node");
   }
 
-  const final: Element[] = [];
+  const final: ChildNode[] = [];
   for (let i = 0; i < result.length; i++) {
     const input = result.item(i);
     if (!input) {
