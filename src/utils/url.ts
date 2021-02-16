467c208ee7a4e6d6dbb053a079ff2d489f0b0dbb
Paul Popat <p.t.popat@googlemail.com>
2021-02-16 22:58:18 +0000

    Moved deno app to the top level

--- src/utils/url.ts
+++
@@ -1,7 +0,0 @@
-export function ParseUrl(url: string) {
-  return url.replace(/\[([a-zA-Z0-9]+)\]/gm, ":$1");
-}
-
-export function RemoveUrlParameters(url: string) {
-  return url.replace(/\[([a-zA-Z0-9]+)\]/gm, "$1");
-}
